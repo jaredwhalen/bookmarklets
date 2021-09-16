11ec617d2a60b60c5f9e2d03c4940ee7b429db04
Jared Whalen <jared.m.whalen@gmail.com>
2021-09-16 13:05:35 -0400

    Create publicS3ToFallback.js

---
+++ scripts/publicS3ToFallback.js
@@ -0,0 +1,17 @@
+//Public S3 → Fallback
+async function copySomething(text) {
+    try {
+        const toCopy = text;
+        await navigator.clipboard.writeText(toCopy);
+    } catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+}
+navigator.clipboard.readText().then(str => {
+    let rx = /https:\/\/graphics\.axios\.com\/(.*)\//g;
+    let slug = rx.exec(str)[1];
+    let fallback = `https://graphics.axios.com/${slug}/fallbacks/${slug}-social.png`;
+    copySomething(fallback);
+}).catch(err => {
+    console.error('Failed to read clipboard contents: ', err);
+})
